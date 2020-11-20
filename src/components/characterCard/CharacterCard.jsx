3213f5d25965da33689fae2fbb812f4dbd173e7d
Eli <jo45840@hotmail.com>
2020-11-20 16:56:42 +0800

    feat: adding media query for characters page in desktop

--- src/components/characterCard/CharacterCard.jsx
+++ src/components/characterCard/CharacterCard.jsx
@@ -5,12 +5,12 @@ import './characterCard.style.scss'
 function CharacterCard({ name, characterImg, intro }) {
   return (
     <div className="characterCard">
-      <div className="front-side">
+      <div className="card__side card__side--front">
         <img src={characterImg} alt="Character" className="characterImg" />
         <div className="triangle triangle-1"></div>
         <div className="triangle triangle-2"></div>
       </div>
-      <div className="back-side">
+      <div className="card__side card__side--back">
         <div className="character__content">
           <h1 className="character__content--name">{name} :</h1>
           <p className="character__content--intro">
