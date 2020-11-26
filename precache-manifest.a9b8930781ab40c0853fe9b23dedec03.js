aef21bfeb94deeaffbbe3ea8af7179aa5658c78a
Eli <jo45840@hotmail.com>
2020-11-26 21:42:29 +0800

    Updates

---
+++ precache-manifest.a9b8930781ab40c0853fe9b23dedec03.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b301b37b5826111c8d3cc4a6876dbe0a",
+    "url": "/highq-web/index.html"
+  },
+  {
+    "revision": "859e1b6e9d82f1bc7c45",
+    "url": "/highq-web/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "e83689cc5a8a97430620",
+    "url": "/highq-web/static/css/main.df13a797.chunk.css"
+  },
+  {
+    "revision": "859e1b6e9d82f1bc7c45",
+    "url": "/highq-web/static/js/2.b344169a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/highq-web/static/js/2.b344169a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e83689cc5a8a97430620",
+    "url": "/highq-web/static/js/main.338dc8d5.chunk.js"
+  },
+  {
+    "revision": "35546de5027d57fc61c1",
+    "url": "/highq-web/static/js/runtime-main.95acbdfa.js"
+  },
+  {
+    "revision": "b119eb94669605e046b0b704c01b1c5e",
+    "url": "/highq-web/static/media/author.b119eb94.svg"
+  },
+  {
+    "revision": "69fff2f5ec3abe361e20c854dea6ceca",
+    "url": "/highq-web/static/media/header-icon.69fff2f5.svg"
+  },
+  {
+    "revision": "e527901f5694afde0233b532f2d53652",
+    "url": "/highq-web/static/media/home-page-long-image.e527901f.jpg"
+  }
+]);
